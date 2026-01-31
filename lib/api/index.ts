7d2b0fcccf9eb0c0be96e7d65266c5cf203ab363
yen115844-ops <yen115844@donga.edu.vn>
2026-01-31 22:23:15 +0700

    first commit

---
+++ lib/api/index.ts
@@ -0,0 +1,13 @@
+export { authApi } from "./auth";
+export { bookingsApi } from "./bookings";
+export { dashboardApi } from "./dashboard";
+export { kycApi } from "./kyc";
+export {
+    districtsApi, interestCategoriesApi, interestsApi, languagesApi, provincesApi, serviceTypesApi, talentCategoriesApi, talentsApi
+} from "./master-data";
+export { notificationsApi } from "./notifications";
+export { partnersApi } from "./partners";
+export { settingsApi } from "./settings";
+export { statisticsApi } from "./statistics";
+export { usersApi } from "./users";
+
